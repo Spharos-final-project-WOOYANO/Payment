aaea56c888865b019c0a22a1297149333d8f17d8
pjm127 <mikeywc40@gmail.com>
2023-10-25 12:52:22 +0900

    feat: 결제 수단,종류 타입 변경

--- src/main/java/shparos/payment/domain/Payment.java
+++ src/main/java/shparos/payment/domain/Payment.java
@@ -1,13 +1,13 @@
 package shparos.payment.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -25,50 +25,36 @@ public class Payment {
     @Column(nullable = false)
     private String clientEmail; //사업자 이메일
     @Column(nullable = false)
-    private String paymentKey; //결제 키
-    @Column(nullable = false)
-    private String orderId; //주문 아이디
-    @Column(nullable = false)
-    private String orderName; //주문명
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private PayType payType; //결제수단 결제수단입니다. 카드, 가상계좌, 간편결제, 휴대폰, 계좌이체
+    @Convert(converter = PaymentTypeConverter.class)
+    private PaymentType paymentType; //결제수단 결제수단입니다. 카드, 간편결제
 
     @Column(nullable = false)
     private int totalAmount; //결제 금액
 
     @Column(nullable = false)
-    private LocalDate requestedAt; //결제가 일어난 날짜와 시간 정보
+    private LocalDateTime approvedAt; //결제 완료,취소가 일어난 날짜와 시간 정보
 
     @Column(nullable = false)
-    private LocalDate approvedAt; //결제 승인이 일어난 날짜와 시간 정보
-
-    @Column(nullable = false)
-    @Enumerated(EnumType.STRING)
-    private PayStatus payStatus; //결제 승인이 일어난 날짜와 시간 정보
+    @Convert(converter = PaymentStatusConverter.class)
+    private PaymentStatus paymentStatus; //결제 완료, 취소, 정산완료
 
 
-
-    private Payment(String clientEmail, String paymentKey, String orderId,
-                    String orderName, PayType payType, int totalAmount,
-                    LocalDate requestedAt, LocalDate approvedAt,PayStatus payStatus) {
+    private Payment(String clientEmail, PaymentType payType, int totalAmount,
+                    LocalDateTime approvedAt, PaymentStatus payStatus) {
         this.clientEmail = clientEmail;
-        this.paymentKey = paymentKey;
-        this.orderId = orderId;
-        this.orderName = orderName;
-        this.payType = payType;
+        this.paymentType = payType;
         this.totalAmount = totalAmount;
-        this.requestedAt = requestedAt;
         this.approvedAt = approvedAt;
-        this.payStatus = payStatus;
+        this.paymentStatus = payStatus;
     }
 
-    public static Payment createPayment(String clientEmail, String paymentKey, String orderId,
-                                        String orderName, PayType payType, int totalAmount,
-                                        LocalDate requestedAt, LocalDate approvedAt,PayStatus payStatus) {
-        return new Payment(clientEmail, paymentKey, orderId, orderName, payType,
-                totalAmount, requestedAt, approvedAt,payStatus);
+    public static Payment createPayment(String clientEmail, PaymentType payType, int totalAmount,
+                                        LocalDateTime approvedAt, PaymentStatus paymentStatus) {
+        return new Payment(clientEmail, payType,
+                totalAmount, approvedAt,paymentStatus);
     }
 
-
+    public void finishSettlement(PaymentStatus paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
 }
