56dec1b107ce512b17bde5da5ace9408f845f3a5
pjm127 <mikeywc40@gmail.com>
2023-10-25 12:55:55 +0900

    feat: 결제 완료,취소 상태 데이터 리스트 메세드 구현, 결제 상태 SETTLED변경 메서드 구현

--- src/main/java/shparos/payment/dto/PaymentRequest.java
+++ src/main/java/shparos/payment/dto/PaymentRequest.java
@@ -1,30 +1,23 @@
 package shparos.payment.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import lombok.Data;
-import shparos.payment.domain.PayStatus;
-import shparos.payment.domain.PayType;
+import shparos.payment.domain.PaymentStatus;
+import shparos.payment.domain.PaymentType;
 
 @Data
 public class PaymentRequest {
     private String clientEmail; //사업자 이메일
-    @Schema(description = "결제 키")
-    private String paymentKey; //있
-    @Schema(description = "주문 아이디")
-    private String orderId; //있
-    @Schema(description = "주문명")
-    private String orderName; //있
+
     @Schema(description = "결제 수단")
-    private PayType payType; //있
+    private PaymentType payType; //있
     @Schema(description = "총 결제 금액")
     private int totalAmount; //있
-    @Schema(description = "결제가 일어난 날짜와 시간 정보")
-    private LocalDate requestedAt; //있
+
     @Schema(description = "결제 승인이 일어난 날짜와 시간 정보")
-    private LocalDate approvedAt; //있
+    private LocalDateTime approvedAt; //있
 
     @Schema(description = "결제 처리 상태")
-    private PayStatus payStatus;
+    private PaymentStatus payStatus;
 }
--- src/main/java/shparos/payment/presentation/PaymentController.java
+++ src/main/java/shparos/payment/presentation/PaymentController.java
@@ -1,12 +1,15 @@
 package shparos.payment.presentation;
 
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import shparos.payment.dto.FinishSettlementRequest;
 import shparos.payment.dto.PaymentRequest;
 import shparos.payment.application.PaymentService;
+import shparos.payment.dto.PaymentResultResponseList;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +26,16 @@ public class PaymentController {
         return ResponseEntity.ok("결제 데이터 저장");
     }
 
+
+    // 결제완료,취소 건들의 리스트 조회
+    @GetMapping("/list")
+    private List<PaymentResultResponseList> getNormalStatus() {
+        return paymentService.getPaymentsList();
+    }
+
+    // 정산 완료시 상태 변경
+    @PostMapping("/finish-settlement")
+    private void finishSettlement(@RequestBody FinishSettlementRequest finishSettlementRequest) {
+        paymentService.finishSettlement(finishSettlementRequest);
+    }
 }
